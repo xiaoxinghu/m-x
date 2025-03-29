e5ee826d4fe21dee7c63f89fa987b3e9441c2a66
Xiaoxing Hu <hi@xiaoxing.dev>
2025-03-30 11:06:38 +1300

    Initial Commit

---
+++ EmacsClient/EmacsClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmacsClientApp.swift
+//  EmacsClient
+//
+//  Created by Xiaoxing Hu on 30/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct EmacsClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EmacsClientTests/EmacsClientTests.swift
@@ -0,0 +1,17 @@
+//
+//  EmacsClientTests.swift
+//  EmacsClientTests
+//
+//  Created by Xiaoxing Hu on 30/03/2025.
+//
+
+import Testing
+@testable import EmacsClient
+
+struct EmacsClientTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
