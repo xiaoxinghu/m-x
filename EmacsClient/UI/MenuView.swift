98083c1c799de4cd6f9b1e4070abaa4b082c83fd
Xiaoxing Hu <hi@xiaoxing.dev>
2026-02-08 11:12:13 +1300

    fix settings ui not brought to front issue

--- EmacsClient/UI/MenuView.swift
+++ EmacsClient/UI/MenuView.swift
@@ -9,10 +9,16 @@ import SwiftUI
 
 struct MenuView: View {
 	@Environment(\.openWindow) var openWindow
-	
+	@Environment(\.openSettings) private var openSettings
+
 	var body: some View {
 		VStack {
-			SettingsLink()
+			Button("Settings...") {
+				// Activate the app first to bring windows to front
+				NSApplication.shared.activate(ignoringOtherApps: true)
+				// Then open settings
+				openSettings()
+			}
 		}
 		.padding()
 	}
